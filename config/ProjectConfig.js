e617eacce46dba966646352c89f2c72b5121974f
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-12-12 18:45:57 +0100

    Eslint disable console rule in config directory

--- config/ProjectConfig.js
+++ config/ProjectConfig.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const path = require('path')
 const glob = require('glob')
 const defu = require('defu')
--- config/WebpackConfig.js
+++ config/WebpackConfig.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const webpack = require('webpack')
 const akaruConfig = require('../akaru.config')
 
