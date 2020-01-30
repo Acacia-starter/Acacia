37ddb6b0e43657915c1493acfcedacd09fadefae
Quentin Neyraud <quentin.neyraud@gmail.com>
2020-01-30 17:52:48 +0100

    Polyfill config

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,5 @@
+import { qs } from '@qneyraud/q-utils'
 import Factory from '~j/lib/Factory'
-
-/**
- * Polyfill
- */
-// https://caniuse.com/#search=intersectionobserver
-import 'intersection-observer'
-
-// Element.closest used in lib https://caniuse.com/#search=element.closest
-// import '@lib/polyfills/closest'
 
 /**
  * Pages
@@ -18,6 +10,10 @@ import Home from '~p/home/index.js'
  * Components
  */
 import Image from '~c/blocks/image/index.js'
+
+/**
+ * Plugins
+ */
 // import ticker from '@lib/plugins/ticker'
 
 /**
@@ -33,13 +29,17 @@ Factory
     image: Image
   })
 
-// Router
-//   .setRootEl(rootElement)
-//   .useNavigo({
-//     homeId: 'home'
-//   })
-//   .start()
+const start = () => {
+  const page = Factory.createPage(qs(document.body, '.page'), {
+    root: document.body
+  })
+  page.createElement()
+  page.init()
+  page.traverse('mounted')
+}
 
-// plugins
-// resize()
-// ticker()
+if (window.polyfilled) {
+  start()
+} else {
+  window.onPolyfill = start
+}
